df3b5ad201a5ac0af04b6944a7f9887a75aee3dc
bersianturi <bsianturiandrean@gmail.com>
2022-10-22 01:17:48 +0700

    newest updates

--- application/views/view-datasiswa.php
+++ application/views/view-datasiswa.php
@@ -1,4 +1,4 @@
-<html> 
+<!-- <html> 
 
 <head>
     <title>Tampil Data Siswa</title> 
@@ -80,11 +80,112 @@
             </tr> 
             <tr>
                 <td colspan="3" align="center"> 
-                    <a href="<?= base_url('datasiswa'); 
-?>">Kembali</a> 
+                    <a href="<?= base_url('datasiswa');
+                                ?>">Kembali</a> 
                 </td>
             </tr> 
         </table> 
     </center> 
 </body> 
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Title Page-->
+    <title>Tampil Data Siswa</title>
+
+    <!-- Icons font CSS-->
+    <link href="<?= base_url('assets') ?>/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="<?= base_url('assets') ?>/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="<?= base_url('assets') ?>/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="<?= base_url('assets') ?>/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="<?= base_url('assets') ?>/css/main.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Tampil Data Siswa</h2>
+                    <form action="<?= base_url() ?>Datasiswa/cetak" method="POST">
+                        <div class="input-group">
+                            <label class="label">Nama Siswa</label>
+                            <h3 class="text-white"> <?= $namasiswa; ?> </h3>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <div class="input-group">
+                                        <label class="label">NIS</label>
+                                        <h3 class="text-white"> <?= $nis; ?> </h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Kelas</label>
+                                    <h3 class="text-white"> <?= $kelas; ?> </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Tempat Lahir</label>
+                                    <h3 class="text-white"> <?= $tmplahir; ?> </h3>
+                                </div>
+                            </div>
+                            <div class=" col-2">
+                                <div class="input-group">
+                                    <label class="label">Tanggal Lahir</label>
+                                    <h3 class="text-white"> <?= $tgllahir; ?> </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <label class="label">Alamat</label>
+                            <h3 class="text-white"> <?= $alamat; ?> </h3>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Agama</label>
+                                    <h3 class="text-white"> <?= $agama; ?> </h3>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <label class="label">Jenis Kelamin</label>
+                                <h3 class="text-white"> <?= $jnskelamin; ?> </h3>
+                            </div>
+                        </div>
+                        <div class="p-t-15">
+                            <a href="<?= base_url('datasiswa'); ?>" class="btn btn--radius-2 btn--blue">Kembali</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="<?= base_url('assets') ?>/vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="<?= base_url('assets') ?>/vendor/select2/select2.min.js"></script>
+    <script src="<?= base_url('assets') ?>/vendor/datepicker/moment.min.js"></script>
+    <script src="<?= base_url('assets') ?>/vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="<?= base_url('assets') ?>/js/global.js"></script>
+
+</body>
+
 </html>
--- application/views/view-form-datasiswa.php
+++ application/views/view-form-datasiswa.php
@@ -1,105 +1,131 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <!-- Title Page-->
     <title>Form Input Data Siswa</title>
+
+    <!-- Icons font CSS-->
+    <link href="<?= base_url('assets') ?>/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="<?= base_url('assets') ?>/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="<?= base_url('assets') ?>/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="<?= base_url('assets') ?>/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="<?= base_url('assets') ?>/css/main.css" rel="stylesheet" media="all">
 </head>
 
 <body>
-    <center>
-        <form action="<?= base_url('Datasiswa/cetak'); ?>" method="post">
-            <table>
-                <tr>
-                    <th colspan="3">
-                        Form Input Data Siswa
-                    </th>
-                </tr>
-                <tr>
-                    <td colspan="3">
-                        <hr>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Nama Siswa</th>
-                    <th>:</th>
-                    <td>
-                        <input type="text" name="namasiswa" id="namasiswa">
-                    </td>
-                </tr>
-                <tr>
-                    <th>NIS</th>
-                    <td>:</td>
-                    <td>
-                        <input type="text" name="nis" id="nis">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Kelas</th>
-                    <td>:</td>
-                    <td>
-                        <select name="kelas" id="kelas">
-                            <option value="">Pilih Kelas</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Tanggal Lahir</th>
-                    <td>:</td>
-                    <td>
-                        <input type="text" name="tgllahir" id="tgllahir">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Tempat Lahir</th>
-                    <td>:</td>
-                    <td>
-                        <input type="text" name="tmplahir" id="tmplahir">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Alamat</th>
-                    <td>:</td>
-                    <td>
-                        <input type="text" name="alamat" id="alamat">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Jenis Kelamin</th>
-                    <td>:</td>
-                    <td>
-                        <select name="jnskelamin" id="jnskelamin">
-                            <option value="">Pilih Jenis Kelamin</option>
-                            <option value="Laki-Laki">Laki-Laki</option>
-                            <option value="Perempuan">Perempuan</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Agama</th>
-                    <td>:</td>
-                    <td>
-                        <select name="agama" id="agama">
-                            <option value="">Pilih Agama</option>
-                            <option value="Islam">Islam</option>
-                            <option value="Kristen">Kristen</option>
-                            <option value="Katolik">Katolik</option>
-                            <option value="Buddha">Buddha</option>
-                            <option value="Hindu">Hindu</option>
-                            <option value="Protestan">Protestan</option>
-                            <option value="Khonghucu">Khonghucu</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="3" align="center">
-                        <input type="submit" value="Submit">
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </center>
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Form Input Data Siswa</h2>
+                    <form action="<?= base_url() ?>Datasiswa/cetak" method="POST">
+                        <div class="input-group">
+                            <label class="label">Nama Siswa</label>
+                            <input class="input--style-4" type="text" name="namasiswa" id="namasiswa">
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <div class="input-group">
+                                        <label class="label">NIS</label>
+                                        <input class="input--style-4" type="number" name="nis" id="nis">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Kelas</label>
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="kelas" id="kelas">
+                                            <option disabled selected hidden>Pilih Kelas</option>
+                                            <option value="12.3A.21">12.3A.21</option>
+                                            <option value="12.3B.21">12.3B.21</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Tempat Lahir</label>
+                                    <input class="input--style-4" type="text" name="tmplahir" id="tmplahir">
+                                </div>
+                            </div>
+                            <div class=" col-2">
+                                <div class="input-group">
+                                    <label class="label">Tanggal Lahir</label>
+                                    <div class="input-group-icon">
+                                        <input class="input--style-4 js-datepicker" type="text" name="tgllahir" id="tgllahir">
+                                        <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <label class="label">Alamat</label>
+                            <input class="input--style-4" type="textarea" name="alamat" id="alamat">
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Agama</label>
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="agama" id="agama">
+                                            <option disabled selected hidden>Pilih Agama</option>
+                                            <option value="Islam">Islam</option>
+                                            <option value="Kristen">Kristen</option>
+                                            <option value="Katolik">Katolik</option>
+                                            <option value="Buddha">Buddha</option>
+                                            <option value="Hindu">Hindu</option>
+                                            <option value="Protestan">Protestan</option>
+                                            <option value="Khonghucu">Khonghucu</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <label class="label">Jenis Kelamin</label>
+                                <div class="p-t-10">
+                                    <label class="radio-container m-r-45">Laki-laki
+                                        <input type="radio" checked="checked" name="jnskelamin" value="Laki-laki">
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="radio-container">Perempuan
+                                        <input type="radio" name="Jenis Kelamin" value="Perempuan">
+                                        <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-t-15">
+                            <button class="btn btn--radius-2 btn--blue" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="<?= base_url('assets') ?>/vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="<?= base_url('assets') ?>/vendor/select2/select2.min.js"></script>
+    <script src="<?= base_url('assets') ?>/vendor/datepicker/moment.min.js"></script>
+    <script src="<?= base_url('assets') ?>/vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="<?= base_url('assets') ?>/js/global.js"></script>
+
 </body>
 
 </html>
